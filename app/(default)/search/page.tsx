50171ce95b9400d8f39f77d6022c6fdbe3187819
dlord213 <lollingworld41@gmail.com>
2025-01-04 18:09:20 +0800

    app: updates on types

--- app/(default)/search/page.tsx
+++ app/(default)/search/page.tsx
@@ -6,6 +6,7 @@ import { useSearchParams } from "next/navigation";
 import Sidebar from "@/components/Sidebar";
 import ProductCard from "@/components/ProductCard";
 import fetchDevicesByQuery from "@/utils/fetchDevicesByQuery";
+import { Suspense } from "react";
 
 export default function Page() {
   const searchParams = useSearchParams();
@@ -18,26 +19,30 @@ export default function Page() {
 
   if (isFetching) {
     return (
-      <main className="flex lg:flex-row gap-4 min-h-screen">
-        <Sidebar />
-        <div className="sticky top-0 flex flex-col gap-4 lg:basis-[70%] lg:p-8 h-full"></div>
-      </main>
+      <Suspense fallback={<div>Loading...</div>}>
+        <main className="flex lg:flex-row gap-4 min-h-screen">
+          <Sidebar />
+          <div className="sticky top-0 flex flex-col gap-4 lg:basis-[70%] lg:p-8 h-full"></div>
+        </main>
+      </Suspense>
     );
   }
 
   return (
-    <main className="flex lg:flex-row gap-4 min-h-screen">
-      <Sidebar />
-      <div className="grid grid-cols-2 gap-8 p-4 lg:grid-cols-6 2xl:grid-cols-5 lg:p-4 lg:gap-4">
-        {data?.map((phone) => (
-          <ProductCard
-            imageSrc={phone.img_src}
-            link={phone.href}
-            product_title={phone.model}
-            key={phone.href}
-          />
-        ))}
-      </div>
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="flex lg:flex-row gap-4 min-h-screen">
+        <Sidebar />
+        <div className="grid grid-cols-2 gap-8 p-4 lg:grid-cols-6 2xl:grid-cols-5 lg:p-4 lg:gap-4">
+          {data?.map((phone) => (
+            <ProductCard
+              imageSrc={phone.img_src}
+              link={phone.href}
+              product_title={phone.model}
+              key={phone.href}
+            />
+          ))}
+        </div>
+      </main>
+    </Suspense>
   );
 }
